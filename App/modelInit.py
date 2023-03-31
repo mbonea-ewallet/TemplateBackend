cd66269d9839779bc63b7dd311b83e64453dd3c9
mbonea-ewallet godwin <godwin.mbonea@ewallet.co.tz>
2023-03-31 09:31:07 +0000

    using a localpostgress db

--- App/modelInit.py
+++ App/modelInit.py
@@ -2,8 +2,12 @@ import databases
 import orm
 import psycopg2
 
+# database = databases.Database(
+#     "postgresql+asyncpg://postgres:CW38stnBqQnCEHe@db.vqbjlhcywjisidxeipdg.supabase.co:5432/postgres"
+# )
 database = databases.Database(
-    "postgresql+asyncpg://postgres:CW38stnBqQnCEHe@db.vqbjlhcywjisidxeipdg.supabase.co:5432/postgres"
+    "postgresql+asyncpg://user:password@db:5432/mydatabase"
 )
+
 # databases = databases.Database(**args)
 models = orm.ModelRegistry(database=database)
